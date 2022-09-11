968b2b095eee6989ecda116dd212a61aef7deda0
LJ <swtw72@gmail.com>
2022-09-11 14:21:52 +0900

    refactor: 여러주스메뉴 주문가능하도록 수정

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -14,24 +14,29 @@ class ViewController: UIViewController {
         let juiceMaker = JuiceMaker()
         
         print("\n딸기바나나 주문")
-        let recipe: Recipe = Recipe.init(menu: "strawberryBananaJuice")
-        let selectedMenu: Array<String> = recipe.strawberryBananaJuice
+        var recipe: Recipe = Recipe.init(menu: "strawberryBananaJuice")
+        var selectedMenu: Array<String> = recipe.strawberryBananaJuice
         
         juiceMaker.orderJuice(selectedMenu: selectedMenu, recipe: recipe)
         
-//        print("\n망고키위주스 주문")
-//        selectedMenu = recipe.mangoKiwiJuice
-//        juiceMaker.orderJuice(selectedMenu: selectedMenu)
-//
-//        print("\n망고주스 주문")
-//        selectedMenu = recipe.mangoJuice
-//        juiceMaker.orderJuice(selectedMenu: selectedMenu)
+        print("\n망고키위주스 주문")
+        recipe = Recipe.init(menu: "mangoKiwiJuice")
+        selectedMenu = recipe.mangoKiwiJuice
+        juiceMaker.orderJuice(selectedMenu: selectedMenu, recipe: recipe)
+
+        print("\n망고주스 주문")
+        recipe = Recipe.init(menu: "mangoJuice")
+        selectedMenu = recipe.mangoJuice
+        juiceMaker.orderJuice(selectedMenu: selectedMenu, recipe: recipe)
+        
+        print("\n딸기주스 주문")
+        recipe = Recipe.init(menu: "strawberryJuice")
+        selectedMenu = recipe.strawberryJuice
+        juiceMaker.orderJuice(selectedMenu: selectedMenu, recipe: recipe)
     }
 }
 
-//        print("\n딸기주스 주문")
-//        var selectedMenu: Array<String> = recipe.strawberryJuice
-//        juiceMaker.orderJuice(selectedMenu: selectedMenu)
+
 //
 //        print("\n바나나주스 주문")
 //        selectedMenu = recipe.bananaJuice
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -10,13 +10,13 @@ struct JuiceMaker {
     
     var fruitStore: FruitStore = FruitStore()           //과일저장소를 소유
     
-    func orderJuice(selectedMenu: Array<String>, recipe: Recipe) {                          //주스 주문
+    func orderJuice(selectedMenu: Array<String>, recipe: Recipe) {                        //주스 주문
         let check: Bool = fruitStore.checkStock(selectedMenu: selectedMenu, recipe: recipe) //재고확인
         if check == true {
             makingJuice(selectedMenu: selectedMenu, recipe: recipe) //주스제조
         }
         resultOfMakingJuice(result: check) //제조결과(성공/실패) 출력
-                print("\n\n수정 후 재고")
+                print("\n수정 후 재고")
                 print("딸기 ",fruitStore.stockOfStrawberry)
                 print("바나나: ",fruitStore.stockOfBanana)
                 print("파인애플: ",fruitStore.stockOfPineapple)
@@ -36,12 +36,3 @@ struct JuiceMaker {
         print("재고소진으로 제조가 불가합니다.")
     }
 }
-
-
-
-//        print("\n\n수정 후 재고")
-//        print("딸기 ",fruitStore.stockOfStrawberry)
-//        print("바나나: ",fruitStore.stockOfBanana)
-//        print("파인애플: ",fruitStore.stockOfPineapple)
-//        print("키위: ",fruitStore.stockOfKiwi)
-//        print("망고: ",fruitStore.stockOfMango)
